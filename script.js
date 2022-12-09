77b18995db7df697e78d5c5d55e38f99f0e8f74b
Sweta kumaeri <swekri59@gmail.com>
2022-12-09 15:12:44 +0530

    final project

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-// const button=document.getElementsByClassName("button")
-// console.log(button)
-const buttonStore=document.getElementsByClassName("button")
-const inputname=document.getElementById("input")
-console.log(inputname)
-console.log(buttonStore)
-function clickonbutton(){
-    inputname.value=buttonStore.innerHTML
-    return inputname.value
-    
+
+// function buttonClicked(number) {
+  let result = document.getElementById("input");
+  console.log(result)
+  let clickonbutton = (number) => {
+    result.value += number;     
+  }
+
+let Result = () => {
+  try {
+    result.value = eval(result.value);
+  }
+  catch (err) {
+    alert("Enter the valid Input");
+  }
+ }
+function clr() {
+  result.value =" ";
 }
 
-
-    
-
-
+function del(){
+  result.value = result.value.slice(0, -1);
+}
+jzx-zggr-ibw
